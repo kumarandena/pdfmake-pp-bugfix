0cda85905318e66e25e24d9d05342224a9da7ada
kumarandena <kumarandena@gmail.com>
2019-05-01 13:54:32 +0530

    added pdf generation logic

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from "@angular/core";
+
+var dialogs = require("tns-core-modules/ui/dialogs");
+import * as pdfMake from 'pdfmake/build/pdfmake.js';
 var clipboard = require("nativescript-clipboard");
 import { FileReaderService } from "../core/fileReader.service";
 
@@ -29,5 +32,35 @@ export class HomeComponent implements OnInit {
                 console.log('Error reading json: ' + JSON.stringify(err));
             }
         )
+    }
+
+    generatePdf() {
+        let docDefinition = {
+            pageOrientation: 'landscape',
+            ownerPassword: '123456',
+            permissions: {
+                printing: 'highResolution',
+                modifying: false,
+                copying: false,
+                annotating: true,
+                fillingForms: true,
+                contentAccessibility: true,
+                documentAssembly: true
+            },
+
+            content: [
+                { text: 'Certificate', fontSize: '25', italics: true, alignment: 'center' },
+            ]
+        };
+
+        pdfMake.createPdf(docDefinition, '', '', this.fonts).getDataUrl((dataUrl) => {
+            dialogs.alert({
+                title: "PDFMake - Base64",
+                message: dataUrl,
+                okButtonText: "Copy to Clipboard"
+            }).then(() => {
+                clipboard.setText(dataUrl);
+            });
+        });
     }
 }
