81324a06821362075bf52a9d78b4a1c0b2f65551
kumarandena <kumarandena@gmail.com>
2019-05-01 13:50:08 +0530

    added clipboard wrapper

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from "@angular/core";
+var clipboard = require("nativescript-clipboard");
+import { FileReaderService } from "../core/fileReader.service";
 
 @Component({
     selector: "Home",
@@ -7,11 +9,25 @@ import { Component, OnInit } from "@angular/core";
 })
 export class HomeComponent implements OnInit {
 
-    constructor() {
+    fonts;
+
+    constructor(private fr: FileReaderService) {
         // Use the component constructor to inject providers.
     }
 
     ngOnInit(): void {
         // Init your component properties here.
+        this.getDatas();
+    }
+
+    getDatas(): void {
+        this.fr.readJSON('/app/core/data.json').then(
+            res => {
+                this.fonts = res["fonts"];
+            },
+            err => {
+                console.log('Error reading json: ' + JSON.stringify(err));
+            }
+        )
     }
 }
